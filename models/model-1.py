0c24bf1c9a211b6088115adb685cc33d0e3771dc
Diego Ramirez <dr01191115@gmail.com>
2024-01-02 18:24:21 -0600

    Set up some initial models and related stuff

---
+++ models/model-1.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+import numpy as np
+
+# prepare data
+celsius = np.array([-40, 10, 0, 8, 15, 22, 38], dtype=float)
+fahrenheit = np.array([-40, 14, 32, 46, 59, 72, 100], dtype=float)
